5759097e9ddca5b38c5efb140ececc3eb9382a0f
Matsko Egor <76002087+fuett966@users.noreply.github.com>
2021-12-24 21:42:57 +0300

    initial commit

---
+++ Assets/Script/GameManagerSingltone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSingltone
+{
+    private static GameManagerSingltone instance;
+
+    private GameManagerSingltone() { }
+
+    public static GameManagerSingltone getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new GameManagerSingltone();
+        }
+        return instance;
+    }
+}
